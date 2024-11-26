4d65e0f2d79459a1a2293884f8a1c529de76950d
SystemX32bits <0000108649622xsp@al.educacao.sp.gov.br>
2024-11-26 12:27:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Viagem Espacial',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota?',
+    'Itália'
+)
+
+criaCartao(
+    'Raciocionio logico',
+    'Oque vale mais ? 1kg de tomate ou 1kg de cebola ?',
+    'os dois sao iguais, ambos pesa 1 kg'
+)
+
+criaCartao(
+    'planetas',
+    ' Qual o maior planeta do sistema solar ? ',
+    ' Júpiter '
+)
+
